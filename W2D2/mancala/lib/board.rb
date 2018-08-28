7df908c75f72dfba6781170ca8489a75006fc0f3
Jingna Li <jingna13@gmail.com>
2018-08-27 18:10:08 -0700

    Finish make_move by moving through the cups correctly

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -1,16 +1,21 @@
+require 'byebug'
+
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
     @cups = Array.new(14) { [] }
-    @cups.each_with_index do |cup, ind|
-      next if ind == 6 || ind == 13
-      cup.concat(Array.new(4, :stone))
-    end
+    place_stones
+    @name1 = name1
+    @name2 = name2
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, ind|
+      next if ind == 6 || ind == 13
+      cup.concat(Array.new(4, :stone))
+    end
   end
 
   def valid_move?(start_pos)
@@ -20,16 +25,31 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
-    if valid_move?(start_pos)
-      stones = @cups[start_pos].count
-      @cups[start_pos] = []
+
+    stones = @cups[start_pos].count
+    @cups[start_pos] = []
+    next_pos = start_pos
+    while stones > 0
+      next_pos = (next_pos + 1) % @cups.length
+
+      next if (current_player_name == @name1 && next_pos == 13) || (current_player_name == @name2 && next_pos == 6)
+      @cups[next_pos].push(:stone)
+      stones -= 1
     end
+
     render
+    next_turn(next_pos)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
-
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -41,10 +61,16 @@ class Board
   end
 
   def one_side_empty?
-    @cups[0..6].all?(&:empty?) || @cups[7..12].all?(&:empty?)
+    @cups[0..5].all?(&:empty?) || @cups[7..12].all?(&:empty?)
   end
 
   def winner
-
+    if @cups[6].count > @cups[13].count
+      @name1
+    elsif @cups[6].count < @cups[13].count
+      @name2
+    else
+      :draw
+    end
   end
 end
