a92094135763b7ed16ca11436cd1a04a8543ec19
Jingna Li <jingna13@gmail.com>
2018-08-30 13:39:28 -0700

    Finish LRU cache implementation

---
+++ W2D5/lru_cache/lru_cache.rb
@@ -0,0 +1,32 @@
+class LRUCache
+
+  def initialize(size=5)
+    @store = []
+    @size = size
+    nil
+  end
+
+  def count
+    @store.count
+  end
+
+  def add(el)
+    @store.delete(el) if @store.include?(el)
+    if count < @size
+      @store << el
+    else
+      @store.shift
+      @store.push(el)
+    end
+    nil
+  end
+
+  def show
+    @store
+  end
+
+
+  private
+  # helper methods go here!
+
+end
