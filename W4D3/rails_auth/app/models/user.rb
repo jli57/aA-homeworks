bfe4650747f920970b34d26a943b5160d86af9ae
Jingna Li <jingna13@gmail.com>
2018-09-11 20:36:22 -0700

    Cleanup code

--- W4D3/rails_auth/app/models/user.rb
+++ W4D3/rails_auth/app/models/user.rb
@@ -12,13 +12,15 @@
 
 class User < ApplicationRecord
   attr_reader :password
-  validates :username, :session_token, presence: true
-  validate :valid_password_digest
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: {message: "Password can't be blank"}
+  validates :session_token, presence: true, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
 
   def self.find_by_credentials(username, pw)
-    password_digest = BCrypt::Password.create(pw)
-    User.find_by(username: username, password_digest: password_digest)
+    user = User.find_by(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(pw)
   end
 
   def self.generate_session_token
@@ -27,9 +29,10 @@ class User < ApplicationRecord
 
   def reset_session_token!
     self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
   end
 
-  before_validation :ensure_session_token
   def ensure_session_token
     self.session_token ||= User.generate_session_token
   end
@@ -40,10 +43,4 @@ class User < ApplicationRecord
     self.password_digest = password_digest
   end
 
-
-  def valid_password_digest
-    if self.password_digest.nil?
-      errors[:password_digest] << "Password can't be blank"
-    end
-  end
 end
