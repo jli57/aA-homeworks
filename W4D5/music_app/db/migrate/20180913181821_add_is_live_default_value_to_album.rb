f43e2cd8632b0c648d30d1b3ce9353dca9ba2029
Jingna Li <jingna13@gmail.com>
2018-09-14 04:27:28 -0700

    Rename to capybara

--- W4D5/music_app/db/migrate/20180913181821_add_is_live_default_value_to_album.rb
+++
@@ -1,5 +0,0 @@
-class AddIsLiveDefaultValueToAlbum < ActiveRecord::Migration[5.2]
-  def change
-    change_column :albums, :is_live, :boolean, default: false
-  end
-end
--- W4D5/music_app/db/migrate/20180913225730_add_optional_to_lyrics_in_tracks.rb
+++
@@ -1,5 +0,0 @@
-class AddOptionalToLyricsInTracks < ActiveRecord::Migration[5.2]
-  def change
-    change_column :tracks, :lyrics, :text, null: true
-  end
-end
