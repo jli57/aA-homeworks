bdfa2ef9b583097391d6e83b21c78479e22cd2ca
Jingna Li <jingna13@gmail.com>
2018-09-16 21:35:12 -0700

    Add Toy model and migrate

--- W5D3/polymorphism-concerns/test/models/toy_test.rb
+++ W5D3/polymorphism-concerns/test/models/toy_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :bigint(8)        not null, primary key
+#  name         :string           not null
+#  toyable_type :string           not null
+#  toyable_id   :bigint(8)        not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class ToyTest < ActiveSupport::TestCase
