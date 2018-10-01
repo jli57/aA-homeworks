de1302fceff46a1630e4ba6cbe4505dee04b39ac
Jingna Li <jingna13@gmail.com>
2018-09-30 21:30:11 -0700

    Add store, action, and reducer

--- W7D1/job_listing/frontend/components/widget.jsx
+++ W7D1/job_listing/frontend/components/widget.jsx
@@ -64,7 +64,7 @@ class Widget extends React.Component {
           Location:
           {cityOptions}
         </div>
-        
+
         <h3>{jobListings.length} Job Listings</h3>
         <ol className="listings-list">
             {jobListings}
--- W7D1/job_listing/frontend/reducer.js
+++ W7D1/job_listing/frontend/reducer.js
@@ -1,10 +1,17 @@
 const initialState = {
-  city: "Please Select", 
+  city: "Please Select",
   jobs: []
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  // return state; // remove this and fill out the body of the reducer function
+  switch(action.type) {
+    case "SWITCH_LOCATION":
+      return { jobs: action.jobs, city: action.city };
+    default:
+      return state;
+  }
+
 };
 
 export default reducer;
