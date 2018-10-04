2fd4af8ce9705366a3fa8d7027369658f1975b1a
Jingna Li <jingna13@gmail.com>
2018-10-03 19:53:31 -0700

    Add Routes and Links

---
+++ W7D4/rainbow_routes/components/red.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Orange from './orange';
+import Yellow from './yellow';
+
+class Red extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2 className="red"></h2>
+        {/* Links here */}
+        <Link to="/red/orange">Orange</Link>
+        <Link to="/red/yellow">Yellow</Link>
+        {/* Routes here */}
+        <Route path="/red/orange" component={Orange}/>
+        <Route path="/red/yellow" component={Yellow}/>
+      </div>
+    );
+  }
+};
+
+export default Red;
